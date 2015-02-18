c35e640f5c98a23b2098478d0c72101234566d4b
reinaldosantos <rsc.mxm@gmail.com>
2015-02-18 14:10:58 -0200

    experimentando data

--- src/AngularJsAspNet5/wwwroot/scripts/Pedido.js
+++ src/AngularJsAspNet5/wwwroot/scripts/Pedido.js
@@ -3,11 +3,10 @@
 
         $http.get("Api/Pedido").success(function (response) {
             $scope.pedido = response;
-            $scope.DataPedidoFormatada = $filter('date')(response.DataPedido, "dd/MM/yyyy");
         });
 
-        $scope.Salvar = function () {
-            $scope.pedido.DataPedido = convertPtBrDateToISODate($scope.DataPedidoFormatada);
+        $scope.salvar = function () {
+            $scope.pedido.DataPedido = convertPtBrDateToISODate($('#dataPedido').val());
 
             $http.post('Api/Pedido', $scope.pedido).success(function (data, status, headers, config) {
                 alert("success");
